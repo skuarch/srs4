49ad0fdcb1e9156a03f178a15c9d30e4540ed814
skuarch <skuarch@yahoo.com.mx>
2012-10-25 17:59:08 -0500

    send and receive allready

--- src/common/RequestDispatcher.java
+++ src/common/RequestDispatcher.java
@@ -2,24 +2,31 @@ package common;
 
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import org.apache.log4j.Logger;
 import util.IOUtilities;
 
 /**
  * Dispatch client.
+ *
  * @author skuarch
  */
 public class RequestDispatcher implements Runnable {
 
     private static final Logger logger = Logger.getLogger(RequestDispatcher.class);
+    private static int client = 0;
     private Socket socket = null;
     private InputStream inputStream = null;
     private ObjectInputStream objectInputStream = null;
+    private OutputStream outputStream = null;
+    private ObjectOutputStream objectOutputStream = null;
 
     //==========================================================================
     /**
      * create a instance.
+     *
      * @param socket Socket
      */
     public RequestDispatcher(Socket socket) {
@@ -37,17 +44,25 @@ public class RequestDispatcher implements Runnable {
 
         try {
 
+            logger.info("attending client " + ++client);
             inputStream = socket.getInputStream();
             objectInputStream = new ObjectInputStream(inputStream);
-            
+
             System.out.println(objectInputStream.readObject());
+
+            outputStream = socket.getOutputStream();
+            objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject("skuarch es tu papa");
+            objectOutputStream.flush();
 
         } catch (Exception e) {
             logger.error(e);
-        }finally{
-            IOUtilities.closeSocket(socket);
+        } finally {
             IOUtilities.closeInputStream(inputStream);
             IOUtilities.closeInputStream(objectInputStream);
+            IOUtilities.closeOutputStream(outputStream);
+            IOUtilities.closeOutputStream(objectOutputStream);
+            IOUtilities.closeSocket(socket);
         }
 
     } // end run    
